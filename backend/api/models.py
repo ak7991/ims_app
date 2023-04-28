fa6ecdf0f835d650978f779010da4c1841a987d7
ak7991_myndsol <akshay.alok@myndsol.com>
2023-04-28 15:30:35 +0530

    Change date to datetime field

--- backend/api/models.py
+++ backend/api/models.py
@@ -22,18 +22,18 @@ class Incident(models.Model):
     
     incident_id = models.CharField(max_length=12, unique=True, editable=False, 
                                    default=create_inc_id, null=False)
-    date_created = models.DateField(auto_now_add=True, null=False, blank=False)
+    date_created = models.DateTimeField(auto_now_add=True, null=False, blank=False, auto_created=True)
     description = models.TextField(max_length=128, null=True, blank=True)
     priority = models.CharField(max_length=20, null=False, choices=PRIORITY, default="LOW")
     user = models.ForeignKey(
         User, null=True, blank=True, on_delete=models.CASCADE)
     closed_status = models.BooleanField(null=False, default=True)
 
-    # def close_incident(self):
-    #     if self.closed_status:
-    #         non_editable_fields = ["date_created", "description", "priority", "user"]
-    #         for field in non_editable_fields:
-    #             self.__getattribute__(field).editable = False
+    def close_incident(self):
+        if self.closed_status:
+            non_editable_fields = ["date_created", "description", "priority", "user"]
+            for field in non_editable_fields:
+                self.__getattribute__(field).editable = False
 
     def __str__(self):
-        return str(self.incident_id) + " - " + str(self.priority) + " - " + str(self.closed_status) + ' date: ' + datetime.strftime(self.date_created,"HH:MM:SS")
+        return str(self.incident_id) + " - " + str(self.priority) + " - " + str(self.closed_status) + ' date: ' + datetime.strftime(self.date_created,"%d/%m/%Y, %H:%M:%S")
